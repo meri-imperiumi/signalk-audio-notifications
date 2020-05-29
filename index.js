672879e0830f7c9d02a248a36d0a367f3b13adcb
Susanna Huhtanen <susanna.huhtanen@iki.fi>
2020-05-29 17:44:05 +0200

    Minor reordering

--- index.js
+++ index.js
@@ -9,47 +9,7 @@ module.exports = (app) => {
   plugin.name = 'signalk-audio-notifications';
   plugin.description = 'Provides SignalK audio notifications';
 
-  let unsubscribes = [];
-  plugin.start = () => {
-    app.debug('audio notifications started');
-    const subscription = {
-      context: 'vessels.self',
-      subscribe: [{
-        path: 'notifications.*',
-        policy: 'instant',
-      }],
-    };
-    app.subscriptionmanager.subscribe(
-      subscription,
-      unsubscribes,
-      (subscriptionError) => {
-        app.error(`Error:${subscriptionError}`);
-      },
-      plugin.handleNotification,
-    );
-    say.speak('Audio notifications activated');
-  };
-
-  plugin.handleNotification = (notification) => {
-    const audibleNotifications = [];
-    notification.updates.forEach((update) => {
-      update.values.forEach((value) => {
-        if (!value.value) {
-          return;
-        }
-        if (!value.value.state || ['alarm', 'emergency'].indexOf(value.value.state) === -1) {
-          return;
-        }
-        if (!value.value.method || value.value.method.indexOf('sound') === -1) {
-          return;
-        }
-        audibleNotifications.push(value);
-      });
-    });
-    return Promise.all(audibleNotifications.map((notify) => plugin.playNotification(notify)));
-  };
-
-  plugin.playNotification = (notification) => new Promise((resolve, reject) => {
+  const playNotification = (notification) => new Promise((resolve, reject) => {
     const notificationFile = path.resolve(__dirname, './assets/notification.mp3');
     player.play(notificationFile, (err) => {
       if (err) {
@@ -67,11 +27,52 @@ module.exports = (app) => {
     });
   });
 
+  const handleNotification = (notification) => {
+    const audibleNotifications = [];
+    notification.updates.forEach((update) => {
+      update.values.forEach((value) => {
+        if (!value.value) {
+          return;
+        }
+        if (!value.value.state || ['alarm', 'emergency'].indexOf(value.value.state) === -1) {
+          return;
+        }
+        if (!value.value.method || value.value.method.indexOf('sound') === -1) {
+          return;
+        }
+        audibleNotifications.push(value);
+      });
+    });
+    return Promise.all(audibleNotifications.map((notify) => playNotification(notify)));
+  };
+
+  let unsubscribes = [];
+  plugin.start = () => {
+    app.debug('audio notifications started');
+    const subscription = {
+      context: 'vessels.self',
+      subscribe: [{
+        path: 'notifications.*',
+        policy: 'instant',
+      }],
+    };
+    app.subscriptionmanager.subscribe(
+      subscription,
+      unsubscribes,
+      (subscriptionError) => {
+        app.error(`Error:${subscriptionError}`);
+      },
+      handleNotification,
+    );
+    say.speak('Audio notifications activated');
+  };
+
   plugin.stop = () => {
     app.debug('audio notifications stopped');
     unsubscribes.forEach((f) => f());
     unsubscribes = [];
     say.speak('Audio notifications deactivated');
   };
+
   return plugin;
 };
